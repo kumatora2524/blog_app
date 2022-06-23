0c2adc8dbe6d407cb9820fdb8c9440f1044e6271
くまとら <ilikehcroy2524@gmail.com>
2022-06-24 07:37:06 +0900

    プロフィールの項目を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,14 +23,44 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
+  delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
     articles.exists?(id: article.id)
   end
-
-  # hayato@gmail.com
+  
   def display_name
-    self.email.split('@').first
-    # => ['hayato', 'gmail.com']
+  #   if profile && profile.nickname
+  #     profile.nickname
+  #   else
+  #     self.email.split('@').first
+  # end
+  
+  # ぼっち演算子
+    profile&.nickname || self.email.split('@').first
+  end
+  # hayato@gmail.com
+  # => ['hayato', 'gmail.com']
+
+  # def birthday
+  #   profile&.birthday
+  # end
+
+  # def gender
+  #   profile&.gender
+  # end
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
   end
 end
